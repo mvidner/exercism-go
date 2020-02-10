33eca4a4877aadde0dadbf966099102c4a07ad4d
Martin Vidner <mvidner@suse.cz>
2020-02-10 15:31:42 +0100

    submit: collatz-conjecture

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,1 +1,25 @@
 package collatzconjecture
+
+import "errors"
+
+// how many Collatz steps from *n* to one; error if *n* isn't positive
+func CollatzConjecture(n int) (int, error) {
+	if n < 1 {
+		return 0, errors.New("input must be at least 1")
+	}
+
+	var steps int = 0
+	for n != 1 {
+		n = CollatzStep(n)
+		steps += 1
+	}
+	return steps, nil
+}
+
+func CollatzStep(i int) int {
+	if i%2 == 0 {
+		return i / 2
+	} else {
+		return 3*i + 1
+	}
+}
