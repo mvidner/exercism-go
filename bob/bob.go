344260ff9dfcbbb235d73561e7db8265714814aa
Martin Vidner <mvidner@suse.cz>
2020-02-10 13:48:24 +0100

    submit: bob

--- bob/bob.go
+++ bob/bob.go
@@ -1,15 +1,36 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package bob
 
-// Hey should have a comment documenting it.
+import "strings"
+
+// Bob's reply to *remark*
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	remark = strings.TrimSpace(remark)
+
+	if remark == "" {
+		return "Fine. Be that way!"
+	}
+
+	shouting := IsShouting(remark)
+	question := IsQuestion(remark)
+	if shouting && question {
+		return "Calm down, I know what I'm doing!"
+	}
+	if shouting {
+		return "Whoa, chill out!"
+	}
+	if question {
+		return "Sure."
+	}
+	return "Whatever."
+}
+
+// There are letters, and they are all in uppercase
+func IsShouting(s string) bool {
+	const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	hasLetters := strings.ContainsAny(s, upper)
+	return hasLetters && s == strings.ToUpper(s)
+}
+
+func IsQuestion(s string) bool {
+	return strings.HasSuffix(s, "?")
 }
