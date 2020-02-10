15d587b5e27009ccc1ce351ca1f96251f64f6970
Martin Vidner <mvidner@suse.cz>
2020-02-10 17:08:03 +0100

    submit: darts

--- darts/darts.go
+++ darts/darts.go
@@ -1,1 +1,17 @@
 package darts
+
+import "math"
+
+func Score(x float64, y float64) int {
+	dist := math.Sqrt(x*x + y*y)
+	if dist > 10 {
+		return 0
+	}
+	if dist > 5 {
+		return 1
+	}
+	if dist > 1 {
+		return 5
+	}
+	return 10
+}
