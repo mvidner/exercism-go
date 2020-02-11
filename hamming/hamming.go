33b6aad4135477cbcb396d3204c92c8f310f7cb7
Martin Vidner <mvidner@suse.cz>
2020-02-11 10:39:46 +0100

    submit: hamming

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,18 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
+	difference_count := 0
+	alen := len(a)
+	blen := len(b)
+	if alen != blen {
+		return 0, errors.New("Distance only works on strings of equal length")
+	}
+	for i := 0; i < alen; i++ {
+		if a[i] != b[i] {
+			difference_count++
+		}
+	}
+	return difference_count, nil
 }
