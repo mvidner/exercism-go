69f30069d11efe926523c892607ca4f1cfe92604
Martin Vidner <mvidner@suse.cz>
2020-02-11 13:28:27 +0100

    submit: robot-name

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -1,1 +1,23 @@
 package robotname
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type Robot struct {
+	name string
+}
+
+func (r *Robot) Name() (string, error) {
+	if r.name == "" {
+		a := 'A' + rand.Intn(26)
+		b := 'A' + rand.Intn(26)
+		n := rand.Intn(1000)
+		r.name = fmt.Sprintf("%c%c%03d", a, b, n)
+	}
+	return r.name, nil
+}
+func (r *Robot) Reset() {
+	r.name = ""
+}
