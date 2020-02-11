9a66ad9d034c39fdf3252f90acbf21239e9408e8
Martin Vidner <mvidner@suse.cz>
2020-02-11 11:50:52 +0100

    submit: parallel-letter-frequency

--- parallel-letter-frequency/parallel_letter_frequency.go
+++ parallel-letter-frequency/parallel_letter_frequency.go
@@ -12,3 +12,28 @@ func Frequency(s string) FreqMap {
 	}
 	return m
 }
+
+func ChanFrequency(ch chan FreqMap, s string) {
+	ch <- Frequency(s)
+}
+
+func UpdateMap(m FreqMap, increment FreqMap) {
+	for k, v := range increment {
+		m[k] += v
+	}
+}
+
+func ConcurrentFrequency(ss []string) FreqMap {
+	m := FreqMap{}
+	ch := make(chan FreqMap)
+	// start
+	for _, s := range ss {
+		go ChanFrequency(ch, s)
+	}
+	// collect
+	for i := 0; i < len(ss); i++ {
+		mm := <-ch
+		UpdateMap(m, mm)
+	}
+	return m
+}
