23ee9781d5c84d11d56def3b53e750710d3384fe
Martin Vidner <mvidner@suse.cz>
2020-02-11 14:20:39 +0100

    robot-name: bonus IMHO works but still fails the test
    go test -tags bonus
    
    IMHO the specification for this exercise sucks, the API is not clear
    from the test cases

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -1,6 +1,7 @@
 package robotname
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 )
@@ -9,15 +10,45 @@ type Robot struct {
 	name string
 }
 
-func (r *Robot) Name() (string, error) {
-	if r.name == "" {
-		a := 'A' + rand.Intn(26)
-		b := 'A' + rand.Intn(26)
-		n := rand.Intn(1000)
-		r.name = fmt.Sprintf("%c%c%03d", a, b, n)
-	}
-	return r.name, nil
+var allRobotNames map[string]bool
+
+func RandomName() string {
+	a := 'A' + rand.Intn(26)
+	b := 'A' + rand.Intn(26)
+	n := rand.Intn(1000)
+	return fmt.Sprintf("%c%c%03d", a, b, n)
 }
+
+func UniqueName() (string, error) {
+	if allRobotNames == nil {
+		allRobotNames = make(map[string]bool)
+	}
+	if len(allRobotNames) == 26 * 26 * 10 * 10 * 10 {
+		return "", errors.New("There are no other robot names left")
+	}
+	for {
+		n := RandomName()
+		known := allRobotNames[n]
+		if !known {
+			allRobotNames[n] = true
+			return n, nil
+		}
+	}
+}
+
+func (r *Robot) Name() (string, error) {
+	var err error
+	if r.name == "" {
+		n, err := UniqueName()
+		if err == nil {
+			r.name = n
+		}
+
+	}
+	return r.name, err
+}
+
+// make *r* forget its name, next r.Name() will give it a new one
 func (r *Robot) Reset() {
 	r.name = ""
 }
