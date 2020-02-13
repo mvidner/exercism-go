8be1352b0ee48e0cd675c1b05e97428fb1c1f9a9
Martin Vidner <mvidner@suse.cz>
2020-02-13 11:16:04 +0100

    submit: series, bonus
    go test -tags first

--- series/series.go
+++ series/series.go
@@ -16,3 +16,11 @@ func All(n int, s string) []string {
 func UnsafeFirst(n int, s string) string {
 	return s[0:n]
 }
+
+// the first substring of s with length n, if possible
+func First(n int, s string) (first string, ok bool) {
+	if n > len(s) {
+		return "", false
+	}
+	return s[0:n], true
+}
