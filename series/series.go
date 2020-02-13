435157019ed3efe4b88fc94462c733df59af7c5b
Martin Vidner <mvidner@suse.cz>
2020-02-13 11:07:06 +0100

    submit: series

--- series/series.go
+++ series/series.go
@@ -1,1 +1,18 @@
 package series
+
+// all the contiguous substrings of length *n* in *s*,
+// in the order that they appear.
+func All(n int, s string) []string {
+	result := []string{}
+
+	end := len(s) - n
+	for start := 0; start <= end; start++ {
+		result = append(result, UnsafeFirst(n, s[start:]))
+	}
+	return result
+}
+
+// the first substring of s with length n
+func UnsafeFirst(n int, s string) string {
+	return s[0:n]
+}
